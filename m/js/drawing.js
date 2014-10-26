000b6e3e7eeb574d2ac400a115eed81f5f9bc551
Kristian Lundström <kristian.lundstrom@gmail.com>
2014-10-26 08:51:31 +0100

    minor changes

--- m/js/drawing.js
+++ m/js/drawing.js
@@ -6,7 +6,6 @@ window.addEventListener('resize', resizeCanvas, false);
     function resizeCanvas() {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
-
             /**
              * Your drawings need to be inside this function otherwise they will be reset when 
              * you resize the browser window and the canvas goes will be cleared.
